f4de0a5020ab7379776a2df3cb719fd838bd9dda
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-03-16 11:37:27 +0600

    added multiple input field handle

--- src/multipleFormInput/MultipleFormInuput.jsx
+++ src/multipleFormInput/MultipleFormInuput.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react";
+
 export default function MultipleFormInput() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <>
       <form>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" />
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <button type="submit">Submit</button>
